6fae665b0aaa2c2d04b703705d9eed53bde3b333
pradeep-maurya02 <pradeep6278068@gmail.com>
2025-03-29 14:56:39 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import "./style.css";
+import products from "./products.json";
 import { showProductContainer } from "./homeProductCards";
 fetch("products.json").then(response => response.json())
 // Define a function named `showProductContainer` that takes an array of products as input.
